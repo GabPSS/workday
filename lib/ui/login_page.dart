947a31a8df5e414f4f2a634a20b76882f5ac975b
GabPSS <45743069+GabPSS@users.noreply.github.com>
2023-12-11 14:19:32 -0300

    Added validation

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:workday/data/app_data.dart';
 import 'package:workday/data/login.dart';
 import 'package:workday/ui/about_dialog.dart';
 import 'package:workday/ui/workday_app.dart';
@@ -16,6 +18,14 @@ class _LoginPageState extends State<LoginPage> {
   String email = "";
   String password = "";
   bool attemptingLogin = false;
+
+  late GlobalKey<FormState> globalKey;
+
+  @override
+  void initState() {
+    super.initState();
+    globalKey = GlobalKey<FormState>();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,80 +68,99 @@ class _LoginPageState extends State<LoginPage> {
               ));
   }
 
-  Column buildLoginContent(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Text(
-            AppLocalizations.of(context)!.welcomeGreeting,
-            style: Theme.of(context).textTheme.titleLarge,
-            textScaleFactor: 1.5,
+  Widget buildLoginContent(BuildContext context) {
+    return Form(
+      key: globalKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              AppLocalizations.of(context)!.welcomeGreeting,
+              style: Theme.of(context).textTheme.titleLarge,
+              textScaleFactor: 1.5,
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            onChanged: (value) => email = value,
-            decoration:
-                InputDecoration(labelText: AppLocalizations.of(context)!.email),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+            child: TextFormField(
+              validator: (value) {
+                if (value == null ||
+                    RegExp(r"\w*@\w*\.\w*").allMatches(value).singleOrNull ==
+                        null) {
+                  return AppLocalizations.of(context)!.invalidEmailMessage;
+                }
+                return null;
+              },
+              onChanged: (value) => email = value,
+              decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.email),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: TextField(
-            obscureText: true,
-            onChanged: (value) => password = value,
-            decoration: InputDecoration(
-                labelText: AppLocalizations.of(context)!.password),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              validator: (value) {
+                if (value?.isEmpty ?? false) {
+                  return AppLocalizations.of(context)!.invalidPasswordMessage;
+                }
+                return null;
+              },
+              obscureText: true,
+              onChanged: (value) => password = value,
+              decoration: InputDecoration(
+                  labelText: AppLocalizations.of(context)!.password),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (attemptingLogin)
-                const CircularProgressIndicator()
-              else
-                Expanded(
-                  child: ElevatedButton.icon(
-                      style: const ButtonStyle(
-                        maximumSize: MaterialStatePropertyAll(Size(200, 80)),
-                      ),
-                      icon: const Icon(Icons.login),
-                      onPressed: () async {
-                        if (email.trim() != "" && password.trim() != "") {
-                          attemptLogin(email, password);
-                        }
-                      },
-                      label: Text(AppLocalizations.of(context)!.loginFunction)),
-                ),
-            ],
-          ),
-        ),
-        if (!attemptingLogin)
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: TextButton.icon(
-                      onPressed: () => showWorkdayAboutDialog(context),
-                      icon: const Icon(Icons.help_outline),
-                      label: Text(
-                          AppLocalizations.of(context)!.aboutAppMenuLabel)),
-                ),
+                if (attemptingLogin)
+                  const CircularProgressIndicator()
+                else
+                  Expanded(
+                    child: ElevatedButton.icon(
+                        style: const ButtonStyle(
+                          maximumSize: MaterialStatePropertyAll(Size(200, 80)),
+                        ),
+                        icon: const Icon(Icons.login),
+                        onPressed: () async {
+                          if (globalKey.currentState?.validate() ?? false) {
+                            attemptLogin(email, password, context);
+                          }
+                        },
+                        label:
+                            Text(AppLocalizations.of(context)!.loginFunction)),
+                  ),
               ],
             ),
-          )
-      ],
+          ),
+          if (!attemptingLogin)
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextButton.icon(
+                        onPressed: () => showWorkdayAboutDialog(context),
+                        icon: const Icon(Icons.help_outline),
+                        label: Text(
+                            AppLocalizations.of(context)!.aboutAppMenuLabel)),
+                  ),
+                ],
+              ),
+            )
+        ],
+      ),
     );
   }
 
-  Future<void> attemptLogin(String email, String password) async {
+  Future<void> attemptLogin(
+      String email, String password, BuildContext context) async {
     setState(() {
       attemptingLogin = true;
     });
@@ -145,6 +174,10 @@ class _LoginPageState extends State<LoginPage> {
           content: Text(AppLocalizations.of(context)!.loginFailedMessage)));
       return;
     }
+
+    if (!mounted) return;
+
+    await Provider.of<AppData>(context, listen: false).fetchData(false);
 
     if (!mounted) return;
 
